7124b9a1e5c272b0a8c6b574e769649d6902f7cd
Xiao Yuan Lv <xiaoyuan.drhorse@gmail.com>
2025-12-08 16:39:34 +0630

    Last Commit

--- islands/Tech.tsx
+++ islands/Tech.tsx
@@ -77,6 +77,81 @@ export default function Tech() {
           </a>
         </div>
       </div>
+
+
+      {/* Project 4 */}
+      <div class="bg-gray-900 rounded-2xl overflow-hidden shadow-lg hover:scale-[1.02] transition-all">
+        <img
+          src="./lunastay.png"
+          alt="Planetz"
+          class="w-[400px] h-[200px] object-cover"
+        />
+        <div class="p-6 text-left">
+          <h3 class="text-2xl font-semibold mb-2 text-yellow-400">
+            Hotel LunaStay
+          </h3>
+          <p class="text-gray-400 text-sm mb-4">
+            A modern hotel reservation template built with PHP Admin and Express API with mobile android.
+          </p>
+          <a
+            href="https://drive.google.com/file/d/1AodkAeaWQ5UwvpdyGAuWg59wEolgDFPR/view?usp=sharing" target="_blank"
+            class="inline-block px-5 py-2 bg-yellow-600 hover:bg-indigo-700 rounded-xl font-medium transition-all"
+          >
+            View Source Code
+          </a>
+        </div>
+      </div>
+
+      {/* Project 5 */}
+      <div class="bg-gray-900 rounded-2xl overflow-hidden shadow-lg hover:scale-[1.02] transition-all">
+        <img
+          src="./expense.png"
+          alt="List"
+          class="w-[400px] h-[300px] object-cover"
+        />
+        <div class="p-6 text-left">
+          <h3 class="text-2xl font-semibold mb-2 text-yellow-400">
+             List
+          </h3>
+          <p class="text-gray-400 text-sm mb-4">
+            A simple and effective To do List application, expenese List etc. built with Flutter.
+          </p>
+          <a
+            href="https://github.com/xiaoyuan31/flutter_projects" target="_blank"
+            class="inline-block px-5 py-2 bg-yellow-600 hover:bg-indigo-700 rounded-xl font-medium transition-all"
+          >
+            View on Github
+          </a>
+        </div>
+      </div>
+
+
+
+
+ {/* Project 6 */}
+      <div class="bg-gray-900 rounded-2xl overflow-hidden shadow-lg hover:scale-[1.02] transition-all">
+        <img
+          src="./weather.png"
+          alt="List"
+          class="w-[400px] h-[300px] object-cover"
+        />
+        <div class="p-6 text-left">
+          <h3 class="text-2xl font-semibold mb-2 text-yellow-400">
+             Weather
+          </h3>
+          <p class="text-gray-400 text-sm mb-4">
+            A simeple weather app built with Flutter.
+          </p>
+          <a
+            href="https://drive.google.com/file/d/1NkXkD0ZVv7-5fSHstyVr6Apo7KOuFkbg/view?usp=sharing" target="_blank"
+            class="inline-block px-5 py-2 bg-yellow-600 hover:bg-indigo-700 rounded-xl font-medium transition-all"
+          >
+            Download Source Code
+          </a>
+        </div>
+      </div>
+
+
     </div>
   </div>
 </section>
